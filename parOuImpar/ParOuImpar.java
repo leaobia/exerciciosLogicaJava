ffaa5ccd6ef372ea73d0a28ca9232c06cab20493
Bianca Leão <biancapeleao@gmail.com>
2024-03-24 21:47:56 -0300

    quarto exercicio realizado

---
+++ parOuImpar/ParOuImpar.java
@@ -0,0 +1,22 @@
+package parOuImpar;
+
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int numero;
+
+        System.out.println("Digite um numero");
+        numero = sc.nextInt();
+
+        if(numero % 2 == 0){
+            System.out.println("Numero par");
+        }else{
+            System.out.println("Numero impar");
+        }
+        
+        sc.close();
+    }
+}
